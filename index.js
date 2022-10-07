81bc61efebad4f09c6411e97c85b53c5197be124
JoannaOyegbile <joannaoyegbile@rocketmail.com>
2022-10-07 11:30:44 +0100

    more changes made

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 const express = require("express");
+
+const mongoose = require('mongoose');
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(req.path);
+    console.log(req.ip);
+    console.log(new Date());
+    next();
+})
+
+//routes
+
+app.use("/array", require("./routes/library.js"));
+app.use("/mongoose", require("./routes/mongoose.js").router);
+
+app.get("/error", (req, res, next) => {
+    next(new Error("Custom Error"));
+})
+
+app.get("/hello", (req, res, next) => {
+    res.status(200).send("Hello!");
+})
+
+/// Error Handling
+app.use((err, req, res, next) => {
+    console.log(err);
+    next(err);
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).send(err.stack);
+    next(err);
+})
+
+// Start
+
+mongoose.connect("mongodb://127.0.0.1:27017").then(()=>{
+    console.log("DB Connected");
+}).catch(console.log);
+
+const server = app.listen(4494, () => {
+    console.log(server.address())
+})
+
+server.on("close", () =>{
+    mongoose.connection.close();
+})
+
+module.exports = {server};
